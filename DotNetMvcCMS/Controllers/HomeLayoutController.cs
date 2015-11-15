c18383cee75895b3cea02d7d20a1add5c1dca9cd
quangpropk <lenhutthanhquang@gmail.com>
2015-11-15 11:39:39 +0700

    Update home layout

---
+++ DotNetMvcCMS/Controllers/HomeLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMvcCMS.Controllers
+{
+    public class HomeLayoutController : Controller
+    {
+        //
+        // GET: /HomeLayout/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Header()
+        {
+            return View();
+        }
+	}
+}
