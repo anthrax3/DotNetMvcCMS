70f9f6186368be686d0323298d7b50448010032f
quangpropk <lenhutthanhquang@gmail.com>
2015-12-06 14:06:07 +0700

    add aceadmin template

---
+++ DotNetMvcCMS/Controllers/AdminLayoutController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMvcCMS.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        //
+        // GET: /AdminLayout/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TopBar()
+        {
+            return View();
+        }
+
+        public ActionResult BreadScrumbs()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+        public ActionResult MainContent()
+        {
+            return View();
+        }
+
+        public ActionResult Setting()
+        {
+            return View();
+        }
+	}
+}
